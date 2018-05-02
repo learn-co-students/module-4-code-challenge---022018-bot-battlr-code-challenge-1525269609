4bc08ae8a52b719b93e47eea8c8cd277f5a74bf2
github username <github email address>
2018-05-02 11:29:59 -0400

    complete disaster

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -3,12 +3,24 @@ import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
   //your code here
+  constructor(){
+    super()
+    this.state = {
+      bots:[]
+    }
+  }
 
   render(){
+    const botCollector = () => {
+      this.state.bots.map(bot=>{
+        <BotCard {...bot}/>
+      })
+    }
+    console.log(botCollector)
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {botCollector}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,28 @@
 import React from "react";
-
+import BotCollection from './BotCollection'
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor(){
+    super()
+    this.state = {
+      bots: []
+    }
+  }
+  botFetch = () => {
+    let url = "https://bot-battler-api.herokuapp.com/api/v1/bots"
+    fetch(url)
+      .then(resp=>resp.json())
+      .then(bots => {
+        this.setState({bots})
+      })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+
+        <BotCollection bots={this.state.bots}/>
+        <button onClick={this.botFetch}/>
       </div>
     );
   }
