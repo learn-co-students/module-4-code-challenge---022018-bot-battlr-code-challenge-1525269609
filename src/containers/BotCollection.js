945b44c6415994a865d57646dc404c19cf1edb5e
github username <github email address>
2018-05-02 11:41:07 -0400

    pushing

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,15 +1,38 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+// import BotSpecs from "../components/BotSpecs";
 
 class BotCollection extends React.Component {
   //your code here
 
+  // state = {
+  //   clicked: false
+  // }
+
+  // toggleClick = () => {
+  //   console.log('in toggleClick');
+  //   const state = this.state.clicked
+  //   this.setState({
+  //     clicked: !state
+  //   })
+  // }
+
   render(){
+
+    const bots = this.props.bots.map((bot, index) => {
+      return <BotCard bot={bot} index={index} key={index} addBot={this.props.addBot} />
+    })
+
+    // const botSpecs = this.props.bots.map((bot, index) => {
+    //   return <BotCard bot={bot} index={index} key={index} toggleClick={this.props.toggleClick} />
+    // })
+
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+          { bots }
     		  Collection of all bots
+          {/* {this.state.clicked ? botSpecs : bots} */}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,59 @@
 import React from "react";
 
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+
+const URL = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
+
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    myBots: [],
+    bots: []
+  }
+
+  fetch = () => {
+    fetch(URL)
+      .then(response => response.json())
+      .then(bots => this.setState({
+        bots: bots
+      }))
+  }
+
+  componentDidMount(){
+    this.fetch()
+  }
+
+  addBot = (addingBot) => {
+    // console.log('in addBot');
+    if(!this.state.myBots.includes(addingBot)){
+      this.setState({
+        myBots: [...this.state.myBots, addingBot]
+      })
+    }
+  }
+
+  deleteBot = (deletingBot) => {
+    // console.log('in deleteBot');
+    const index = this.state.myBots.indexOf(deletingBot)
+    const newArray = [...this.state.myBots]
+    newArray.splice(index, 1)
+    this.setState({
+      myBots: newArray
+    })
+  }
+
+
   render() {
+
+    // console.log(this.state.bots);
+    console.log(this.state.myBots);
+
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy myBots={this.state.myBots} deleteBot={this.deleteBot}/>
+        <BotCollection bots={this.state.bots} addBot={this.addBot} />
       </div>
     );
   }
